92405fd94784213a038ccdd7fffeb98d2b2339c5
Rakesh <kgrakesh@outlook.com>
2023-03-06 10:40:11 +0530

    Add project files.

---
+++ testmk/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace testmk.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
